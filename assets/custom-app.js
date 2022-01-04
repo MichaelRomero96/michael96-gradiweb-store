64cd7453ce578410d3e29d21c2bab43884df355a
ekimaru <michaelporshe64@gmail.com>
2022-01-04 18:04:29 -0500

    Setting variant functions

--- assets/custom-app.js
+++ assets/custom-app.js
@@ -1,1 +1,43 @@
-console.log('hello js')
+let loaded = false;
+
+let variantBtnProduct;
+
+class productVariants {
+    constructor(idElement) {
+        this.idElement = idElement;
+    }
+    variantListener() {
+        this.allImagesList();
+        this.allVariantsList().forEach((element, index) => {
+            element.addEventListener('click', () => {
+                this.changeProductImage(element)
+            })
+        });
+    };
+    changeProductImage(element) {
+        this.allImagesList();
+        console.log(element)
+    }
+    allVariantsList() {
+        const elementsNodeList = document.querySelectorAll(this.idElement);
+        return Array.from(elementsNodeList);
+    }
+    allImagesList() {
+        const elementsNodeListImages = document.querySelectorAll('img');
+        return Array.from(elementsNodeListImages);
+    }
+
+};
+
+const variantElementButton = new productVariants('#variant-btn');
+
+window.addEventListener('DOMContentLoaded', (e) => {
+    console.log('entrando');
+    variantElementButton.variantListener();
+    variantBtnProduct = document.querySelector('#variant-btn');
+});
+
+
+
+
+
