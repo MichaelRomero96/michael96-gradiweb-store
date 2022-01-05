0d69d3ee1857cab728d0ae88ebbecee7dc193d38
michael <msromeroma@gmail.com>
2022-01-04 23:26:11 -0500

    Adjust hover script

--- assets/custom-app.js
+++ assets/custom-app.js
@@ -1,38 +1,40 @@
-let loaded = false;
-
-let variantBtnProduct;
-
 class productVariants {
     constructor(idElement) {
         this.idElement = idElement;
-    }
+    };
     variantListener() {
         this.allImagesList();
         this.allVariantsList().forEach((element, index) => {
             element.addEventListener('click', () => {
                 this.changeProductImage(element)
-            })
+            });
         });
     };
     changeProductImage(element) {
-        this.allImagesList();
-        console.log(element)
-    }
+        const filteredImgByProduct = this.allImagesList().filter(i => i.attributes.id_product.value === element.attributes.id_product.value);
+        if (filteredImgByProduct.length !== 0) {
+            filteredImgByProduct.forEach((img, index) => {
+                if (index === parseInt(element.attributes.variant_index.value)) {
+                    img.classList.remove('img-hide')
+                } else {
+                    img.classList.add('img-hide')
+                }
+            });
+        };
+    };
     allVariantsList() {
         const elementsNodeList = document.querySelectorAll(this.idElement);
         return Array.from(elementsNodeList);
-    }
+    };
     allImagesList() {
         const elementsNodeListImages = document.querySelectorAll('img');
         return Array.from(elementsNodeListImages);
-    }
-
+    };
 };
 
 const variantElementButton = new productVariants('#variant-btn');
 
 window.addEventListener('DOMContentLoaded', (e) => {
-    console.log('entrando');
     variantElementButton.variantListener();
     variantBtnProduct = document.querySelector('#variant-btn');
 });
